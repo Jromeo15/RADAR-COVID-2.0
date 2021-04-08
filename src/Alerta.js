6281e4e1f61f7a61d0bde141525c4d85bedfcac0
albertomgranda <albertomgranda@gmail.com>
2021-04-08 13:47:19 +0200

    Alerta

---
+++ src/Alerta.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+}
+from "react-router-dom";
+import Registro from "./Registro";
+import MenuPrincipal from "./MenuPrincipal";
+
+function Alerta() {
+  return (
+
+
+    <div className="Alerta">
+        <div className="fondo">
+<head>
+	
+	<title>Radar Covid</title>
+
+
+</head>
+
+<body>
+	
+	
+   
+		<div className="title">
+			<div className="titulo"><h1>Radar COVID</h1></div>
+			<p className="spacer"></p>
+			<div className="titulillo"><h2>Enviar un reporte</h2></div>
+			<p className="spacer"></p>
+			
+		</div>
+		<div className="registro">
+			<div className="insertar">
+				<p className="spacer"></p>
+				<Link to="/MenuPrincipal"><button class="botonM">Menu principal</button></Link>
+				<p className="spacer"></p>
+				<div className="cuadro">
+				<p>Atención, está a punto de enviar un informe al Ministerio de Sanidad. Sus datos serán enviados al servidor. ¿Está seguro de que desea continuar? </p>
+				<p className="spacer"></p>
+				<span className="cuadro"v><Link to="/MenuPrincipal">SÍ</Link></span>  <span className="cuadror"><Link to="/Reporte">NO</Link></span>
+				<p className="spacer"></p>
+				</div>
+				<p className="spacer"></p>
+			
+			</div>
+		</div>
+
+</body>
+</div>
+    </div>
+  );
+}
+
+export default Alerta;
