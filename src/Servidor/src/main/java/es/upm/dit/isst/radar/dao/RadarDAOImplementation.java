e75e694198a3ea21bcf744501a9f4791893965aa
Miguel23dejunio <61226578+Miguel23dejunio@users.noreply.github.com>
2021-04-13 17:21:26 +0200

    cambios eclipse 2

--- src/Servidor/src/main/java/es/upm/dit/isst/radar/dao/RadarDAOImplementation.java
+++ src/Servidor/src/main/java/es/upm/dit/isst/radar/dao/RadarDAOImplementation.java
@@ -1,11 +1,13 @@
 package es.upm.dit.isst.radar.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
 
 import es.upm.dit.isst.radar.model.RegistroInfectados;
 import es.upm.dit.isst.radar.model.Usuario;
+import es.upm.dit.isst.radar.dao.SessionFactoryService;
 
 public class RadarDAOImplementation implements RadarDAO{
 
@@ -43,18 +45,9 @@ public class RadarDAOImplementation implements RadarDAO{
 			  return usuario;
 			 
 		 }
-	/*
-			public TFG read(String email) {}
+		
+		
 	
-			public TFG update(TFG tfg{}
-	
-			public TFG delete(TFG tfg) {}
-	
-			public List<TFG> readAll(){}
-	
-			public List<TFG> readAll(String professor){}
-*/
-
 		@Override
 		public RegistroInfectados create(RegistroInfectados registroInfectados) {
 			// TODO Auto-generated method stub
@@ -63,32 +56,53 @@ public class RadarDAOImplementation implements RadarDAO{
 
 		@Override
 		public Usuario read(String email) {
-			// TODO Auto-generated method stub
-			return null;
+			Session session = SessionFactoryService.get().openSession();
+			session.beginTransaction();
+			Usuario usuario = session.get(Usuario.class, email);
+			session.getTransaction().commit();
+			session.close();
+			return usuario;
 		}
 
 		@Override
 		public Usuario update(Usuario usuario) {
-			// TODO Auto-generated method stub
-			return null;
+			Session session = SessionFactoryService.get().openSession();
+			session.beginTransaction();
+			session.saveOrUpdate(usuario);
+			session.getTransaction().commit();
+			session.close();
+			return usuario;
 		}
 
 		@Override
 		public Usuario delete(Usuario usuario) {
-			// TODO Auto-generated method stub
-			return null;
+			Session session = SessionFactoryService.get().openSession();
+			session.beginTransaction();
+			session.delete(usuario);
+			session.getTransaction().commit();
+			session.close();
+			return usuario;
 		}
 
+		@SuppressWarnings("unchecked")
 		@Override
 		public List<Usuario> readAll() {
-			// TODO Auto-generated method stub
-			return null;
+			List<Usuario> usuarios = new ArrayList<Usuario> ();
+			Session session = SessionFactoryService.get().openSession();
+			session.beginTransaction(); 
+			usuarios.addAll(session.createQuery("from Usuario").list());
+			session.getTransaction().commit();
+			session.close();
+			return usuarios;
 		}
 
 		@Override
 		public List<Usuario> readAll(int DNI) {
-			// TODO Auto-generated method stub
-			return null;
+			List<Usuario> res = new ArrayList<Usuario>();
+			/*for (Usuario usuario : this.readAll())
+				if (usuario.getAdvisor().equals(professor))
+					res.add(usuario);*/
+			return res;
 		}
 	  
 }
