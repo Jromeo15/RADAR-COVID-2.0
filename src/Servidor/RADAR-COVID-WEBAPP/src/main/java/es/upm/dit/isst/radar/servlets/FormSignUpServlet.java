6876b4cf4db8aea1fe754396cc789fac721568c4
Pablo Muñoz <pablommd99@gmail.com>
2021-05-16 12:29:29 +0200

    Comprobacion DNI

--- src/Servidor/RADAR-COVID-WEBAPP/src/main/java/es/upm/dit/isst/radar/servlets/FormSignUpServlet.java
+++ src/Servidor/RADAR-COVID-WEBAPP/src/main/java/es/upm/dit/isst/radar/servlets/FormSignUpServlet.java
@@ -26,13 +26,25 @@ import es.upm.dit.isst.radar.dao.*;
 public class FormSignUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-       
+	public static boolean isNumeric(String str) { 
+	    try {  
+	      Double.parseDouble(str);  
+	      return true;
+	    } catch(NumberFormatException e){  
+	      return false;  
+	    }  
+	  }
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
 		
 		String email = req.getParameter("email");
-		String DNI = req.getParameter("DNI");			
+		String DNI = req.getParameter("DNI");
+		if (DNI.length()!=9 || !isNumeric(DNI.substring(0, 7)) || isNumeric(DNI.substring(7))) {
+            getServletContext().getRequestDispatcher("/Registro.jsp").forward(req,resp);
+            return;
+        }
 		String password = req.getParameter("password");
 		boolean ministerio = Boolean.parseBoolean( req.getParameter("ministerio") );
 		
